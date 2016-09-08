85251d944855beb1f3fba1fc9fa54b668500bb53
Hưng Đào <zocoi@users.noreply.github.com>
2016-09-08 15:30:52 -0700

    Update sidekiq-pubsub.rb

--- lib/sidekiq-pubsub.rb
+++ lib/sidekiq-pubsub.rb
@@ -6,6 +6,12 @@
 #   on 'rectify_claims', RectifyClaimsWorker
 #   on 'rectify_claims', ClaimRenewalJob
 # end
+#
+# Sidekiq.subscribe do
+#   on 'rectify_claims', (event) -> {
+#     RectifyClaimsWorker.perform_async(event.args, extra_options={})
+#   }
+# end
 
 module Sidekiq
   def self.publish(event_name, payload={})
@@ -23,7 +29,7 @@ module Sidekiq
   def self.subscribe
     yield self if block_given?
   end
-  def self.on(event_name, job_klass)
+  def self.on(event_name, job_klass_or_lambda)
       # Need to control this subcriber later
       subscriber = ActiveSupport::Notifications.subscribe(event_name) do |*args|
         event = ActiveSupport::Notifications::Event.new(*args)
